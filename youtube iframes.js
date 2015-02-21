76d6dc24ca1a42557f31a31ef8a23543a8e02276
r57zone <r57zone@gmail.com>
2015-02-21 13:00:11 +0400

    'update'

--- youtube iframes.js
+++ youtube iframes.js
@@ -13,7 +13,11 @@ for (i = 0; i < frames.length; ++i)
   {
     if (frames[i].src.indexOf('youtube.com/')!=-1) {
      var ytplayer=document.createElement('div');
-     ytplayer.innerHTML='<object type="application/x-shockwave-flash" style="width:537px; height:302px;" data="'+frames[i].src.replace('/embed/','/v/')+'">'+"\r\n"+'<param name="movie" value="'+frames[i].src.replace('/embed/','/v/')+'" />'+"\r\n"+'<param name="allowFullScreen" value="true" />'+"\r\n"+'<param name="allowscriptaccess" value="always" />'+"\r\n"+'</object>';
+     var ytwidth=frames[i].getAttribute('width');
+     var ytheight=frames[i].getAttribute('height');
+       if (ytwidth.indexOf('%')!=-1) {ytwidth='537px';} else {ytwidth=ytwidth+'px';}
+       if (ytheight.indexOf('%')!=-1) {ytheight='302px';} else {ytheight=ytheight+'px';}
+     ytplayer.innerHTML='<object type="application/x-shockwave-flash" style="width:'+ytwidth+'; height:'+ytheight+';" data="'+frames[i].src.replace('/embed/','/v/')+'">'+"\r\n"+'<param name="movie" value="'+frames[i].src.replace('/embed/','/v/')+'" />'+"\r\n"+'<param name="allowFullScreen" value="true" />'+"\r\n"+'<param name="allowscriptaccess" value="always" />'+"\r\n"+'</object>';
      frames[i].parentNode.appendChild(ytplayer);
      frames[i].parentNode.removeChild(frames[i]);
     }
