0c216c413b3f2d4addc1edb364189e428d39a852
r57zone <r57zone@gmail.com>
2015-03-03 16:24:19 +0400

    update

--- youtube iframes.js
+++ youtube iframes.js
@@ -2,7 +2,7 @@
 // @name        YouTube iframes
 // @namespace   YouTube_iframes
 // @include     *
-// @version     0.1
+// @version     0.2
 // @grant       none
 // ==/UserScript==
 
@@ -15,9 +15,11 @@ for (i = 0; i < frames.length; ++i)
      var ytplayer=document.createElement('div');
      var ytwidth=frames[i].getAttribute('width');
      var ytheight=frames[i].getAttribute('height');
-       if (ytwidth.indexOf('%')!=-1) {ytwidth='537px';} else {ytwidth=ytwidth+'px';}
-       if (ytheight.indexOf('%')!=-1) {ytheight='302px';} else {ytheight=ytheight+'px';}
-     ytplayer.innerHTML='<object type="application/x-shockwave-flash" style="width:'+ytwidth+'; height:'+ytheight+';" data="'+frames[i].src.replace('/embed/','/v/')+'">'+"\r\n"+'<param name="movie" value="'+frames[i].src.replace('/embed/','/v/')+'" />'+"\r\n"+'<param name="allowFullScreen" value="true" />'+"\r\n"+'<param name="allowscriptaccess" value="always" />'+"\r\n"+'</object>';
+     var ytsrc=frames[i].src.replace('/embed/','/v/');
+     if (ytwidth.indexOf('%')!=-1) {ytwidth='537px';} else {ytwidth=ytwidth+'px';}
+     if (ytheight.indexOf('%')!=-1) {ytheight='302px';} else {ytheight=ytheight+'px';}
+     if (ytsrc.indexOf('?')!=-1) {ytsrc=ytsrc.substr(0,ytsrc.indexOf('?')); ytsrc=ytsrc+'?autoplay=0';} else {ytsrc=ytsrc+'?autoplay=0';}
+     ytplayer.innerHTML='<object type="application/x-shockwave-flash" style="width:'+ytwidth+'; height:'+ytheight+';" data="'+ytsrc+'">'+"\r\n"+'<param name="movie" value="'+frames[i].src.replace('/embed/','/v/')+'" />'+"\r\n"+'<param name="allowFullScreen" value="true" />'+"\r\n"+'<param name="allowscriptaccess" value="always" />'+"\r\n"+'</object>';
      frames[i].parentNode.appendChild(ytplayer);
      frames[i].parentNode.removeChild(frames[i]);
     }
@@ -25,5 +27,4 @@ for (i = 0; i < frames.length; ++i)
  }
 
 updateframes();
-
 window.onscroll = function() {updateframes();}
